1e4a641a1b5fe8d6112dafba536cfc80886e7617
c9dong <davidCdong_123@hotmail.com>
2016-09-18 18:57:49 -0400

    sessions

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  def self.findByUsername(username)
+    return User.where(name: username)
+  end
 end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :users
   root 'application#welcome'
+
+  # sessions
+  post 'login' => 'session#create'
+  post 'logout' => 'session#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
